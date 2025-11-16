0c837d66428748f0b6863e2a7a358a6eace791e8
Andrii Pohuliailo <pogulailo.a@gmail.com>
2025-11-16 17:16:52 +0200

    Switch from test model to real uploaded model

--- app/src/Controller/QuoteController.php
+++ app/src/Controller/QuoteController.php
@@ -4,19 +4,39 @@ namespace App\Controller;
 
 use App\Dto\Request\QuoteRequestDto;
 use App\UseCase\CalculateQuoteUseCase;
+use InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuoteController extends AbstractController
+final class QuoteController extends AbstractController
 {
     #[Route('/quotes', name: 'quotes_create', methods: ['POST'])]
     public function create(
-        #[MapRequestPayload] QuoteRequestDto $quoteRequest,
+        Request $request,
         CalculateQuoteUseCase $calculateQuoteUseCase,
+        Filesystem $filesystem,
     ): JsonResponse {
+        $model = $request->files->get('model');
+
+        if (!$model instanceof UploadedFile) {
+            throw new InvalidArgumentException('Model must be uploaded');
+        }
+
+        $newFilePath = $model->getRealPath() . '.' . $model->getClientOriginalExtension();
+        $filesystem->copy($model->getRealPath(), $newFilePath);
+
+        $quoteRequest = new QuoteRequestDto(
+            $request->request->get('printerProfile'),
+            $request->request->get('printProfile'),
+            $request->request->get('materialProfile'),
+            $newFilePath,
+        );
+
         $responseDto = $calculateQuoteUseCase->handle($quoteRequest);
 
         return $this->json($responseDto, Response::HTTP_OK);
